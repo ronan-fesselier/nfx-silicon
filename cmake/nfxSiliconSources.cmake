b59d91613fc59f2ecf6edd3c50f747e2832de039
Ronan F․ <ronan.fesselier@etik.com>
2026-07-05 08:12:36 +0200

    feat(gate/cd4xxx): add CD4049UB CMOS hex inverting buffer

--- cmake/nfxSiliconSources.cmake
+++ cmake/nfxSiliconSources.cmake
@@ -35,6 +35,7 @@ set(NFX_SILICON_SOURCES
 
     # CD4xxx packages
     src/gate/cd4xxx/CD4011B.cpp
+    src/gate/cd4xxx/CD4049UB.cpp
 
     # 74xx packages
     src/gate/sn74/LS00.cpp
--- include/nfx/Silicon.h
+++ include/nfx/Silicon.h
@@ -49,6 +49,7 @@
 
 // CD4xxx packages
 #include "nfx/silicon/gate/cd4xxx/CD4011B.h"
+#include "nfx/silicon/gate/cd4xxx/CD4049UB.h"
 
 // 74xx packages
 #include "nfx/silicon/gate/sn74xxx/LS00.h"
